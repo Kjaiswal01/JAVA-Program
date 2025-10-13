84bca30b405066ae482ba40c81587558b50868e0
Kjaiswal01 <kshitijj42@gmail.com>
2025-10-13 21:26:49 +0530

    Create EvenOdd.java

---
+++ CONTROL_STATEMENTS_PROGRAMS/EvenOdd.java
@@ -0,0 +1,12 @@
+package CONTROL_STATEMENTS_PROGRAMS;
+
+class EvenOdd {
+    public static void main(String[] args) {
+        int no = 12;
+        if (no % 2 == 0) {
+            System.out.println("number " + no + " is even");
+        } else {
+            System.out.println("number " + no + " is odd");
+        }
+    }
+}
