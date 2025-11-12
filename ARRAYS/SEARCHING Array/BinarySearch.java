11d0bca063f09390678adf12cde967acb6219c36
Kjaiswal01 <kshitijj42@gmail.com>
2025-11-12 22:54:46 +0530

    Create BinarySearch.java

---
+++ ARRAYS/SEARCHING Array/BinarySearch.java
@@ -0,0 +1,26 @@
+package SEARCHING;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] a = { 2, 5, 7, 9, 10, 13, 15, 19, 26, 29 };
+        int srch = 13;
+        int li = 0;
+        int hi = a.length - 1;
+        int mi = (li + hi) / 2;
+
+        while (li <= hi) {
+            if (a[mi] == srch) {
+                System.out.println("element is at " + mi + " index position");
+                break;
+            } else if (a[mi] < srch) {
+                li = mi - 1;
+            } else {
+                hi = mi - 1;
+            }
+            mi = (li + hi) / 2;
+        }
+        if (li > hi) {
+            System.out.println("element not found");
+        }
+    }
+}
